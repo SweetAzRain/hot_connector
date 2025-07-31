d880a0168c1d10bb04a722c75e871e6a39d6b6a7
Qelmi <33161152+SweetAzRain@users.noreply.github.com>
2025-07-31 06:21:47 +0300

    Update WalletModal.tsx

--- src/ui-selector/WalletModal.tsx
+++ src/ui-selector/WalletModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useState } from "react/hooks";
 
 import { InjectedWallet } from "../wallets/InjectedWallet";
 import { WalletSelector } from "../selector";
